a969f01f162ffe4eb22a63b38ebb10938548b473
Jakub Pavlík <severus@post.cz>
2017-10-27 20:37:57 +0200

    season weeks

--- spec/10_season_spec.rb
+++ spec/10_season_spec.rb
@@ -1,9 +1,9 @@
 RSpec.describe 'Season' do
-  let(:comparison) do
-    CR::Spec::Comparison.new SPECLI, slice: [:season]
-  end
-
   describe 'limits' do
+    let(:comparison) do
+      CR::Spec::Comparison.new SPECLI, slice: [:season]
+    end
+
     %i(first_advent_sunday nativity baptism_of_lord ash_wednesday pentecost).each do |day|
       describe day do
         date = CR::Temporale::Dates.public_send(day, TEST_YEAR)
@@ -21,5 +21,22 @@ RSpec.describe 'Season' do
     end
   end
 
-  describe 'mass test'
+  describe 'weeks' do
+    let(:comparison) do
+      CR::Spec::Comparison.new SPECLI, slice: [:season, :season_week]
+    end
+
+    (Date.new(TEST_YEAR, 1, 1) .. Date.new(TEST_YEAR, 12, 31))
+      .each do |date|
+
+      # we don't need to check all days
+      next unless date.sunday? || # edge
+                  date.monday? || # non-edge
+                  date.saturday? # edge
+
+      describe date do
+        it { expect(comparison).to match_for date }
+      end
+    end
+  end
 end
